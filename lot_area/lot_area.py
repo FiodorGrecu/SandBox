102d8b2580259ea62e3108723313d5d2073a39e1
FiodorG <gre_dorelus@yahoo.com>
2021-04-21 18:47:20 -0400

    Lot area mesurement

---
+++ lot_area/lot_area.py
@@ -0,0 +1,11 @@
+# Code your solution here
+# length = float(input("Enter your lenght in miles: "))
+length = 6
+# bredth = float(input("Enter your bredth in miles: "))
+breadth = 4
+data = length * breadth
+length = float(input("Enter your lenght in miles: "))
+
+bredth = float(input("Enter your bredth in miles: "))
+data = length * bredth
+print(f"You're area is {data} square feet ! ")
