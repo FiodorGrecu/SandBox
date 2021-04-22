9f8f125624cbd290f5d82cc6382c5ebe5e658ea1
FiodorG <gre_dorelus@yahoo.com>
2021-04-21 20:37:50 -0400

    Sum list and Read me file

---
+++ sum_list/sum_list.py
@@ -0,0 +1,9 @@
+list_a = [1.2, 3.04, 2, 55]
+total = []
+total.append(sum(list_a))
+for i in range(len(list_a)):
+    if list_a[i] == int:
+        print(list_a[i])
+    else:
+        print(type(list_a[i]))
+print(total)
