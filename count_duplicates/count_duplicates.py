7096b1499f689f642e3b74405e51bc8bd6d779d6
FiodorG <gre_dorelus@yahoo.com>
2021-04-26 12:55:07 -0400

    Count duplicates with Counter function

--- count_duplicates/count_duplicates.py
+++ count_duplicates/count_duplicates.py
@@ -6,3 +6,13 @@ for i in list_dup:
         total += 1
 tot.append(total)
 print(tot)
+
+
+# Now I will  ry with the COUNTER function to see the results
+
+# from collections import Counter
+
+# list_dup = ['a', 'c', 'b','r', 't', 'c', 't','a', 'c',]
+# dupes_times = Counter(list_dup)
+# print(list_dup)
+# print(dupes_times)
