338f6c9b9b04dd0211e3753fb8931ae436c05d2b
FiodorG <gre_dorelus@yahoo.com>
2021-05-05 12:23:45 -0400

    Palindrome exercise

--- Data_Types_And_Control_Flow/3.palindrome/palindrome.py
+++ Data_Types_And_Control_Flow/3.palindrome/palindrome.py
@@ -1,7 +1,11 @@
 
 line = input('Enter a word and I will check if is paindrome: ')
+is_palindrome = []
 
 if line == line[::-1]:
+    is_palindrome.append(line[::-1])
     print('This word is palindrome')
 else:
-    print('This word is not palindrome')
+    pass
+
+print(is_palindrome)
