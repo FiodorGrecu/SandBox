9e7543868ce7e0108ba50480d9930db2548d5852
FiodorG <gre_dorelus@yahoo.com>
2021-05-17 14:49:58 -0400

    Updated solution to max_val

--- Fucntions/2.max_val/max_val.py
+++ Fucntions/2.max_val/max_val.py
@@ -1,12 +1,10 @@
 a = int(input("Number a "))
 b = int(input("Number b "))
 c = int(input("Number c "))
-# b = 2
-# c = 5
 
 def max_value(a,b,c):
     result = max(a,b,c)
     return result
     
 print(max_value(a,b,c))
-# print(result(2,3,4))
+print(max_value(44,77,99))
