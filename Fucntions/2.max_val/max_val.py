1243a1482305acbb7de7e2b195211d0206615621
FiodorG <gre_dorelus@yahoo.com>
2021-05-17 14:37:00 -0400

    Max val exercise solution

---
+++ Fucntions/2.max_val/max_val.py
@@ -0,0 +1,9 @@
+a = 1
+b = 2
+c = 5
+
+def max_value(int(input("number a")), int(input("number b")), int(input("number c"))):
+    return max(a,b,c)
+    result = max_value()
+print(max_value(4,6,8))
+# print(result(2,3,4))
