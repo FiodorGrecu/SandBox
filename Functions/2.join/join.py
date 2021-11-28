fc71442990a1e0486c11ade70cdb31ba54e6ca8d
FiodorG <gre_dorelus@yahoo.com>
2021-11-27 21:04:42 -0500

    Refresher

--- Functions/2.join/join.py
+++ Functions/2.join/join.py
@@ -2,7 +2,5 @@ lst = ['APPLE', 'banana', 'CUCUMBER']
 
 joined = ", ".join(lst)
 joined_new = "".join(lst)
-print(joined.upper())
-print(joined.lower())
 print(joined_new.lower())
 print(lst.count(lst))
