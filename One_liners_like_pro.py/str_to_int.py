bbba04f0b73ab4f15c53e98b0ab69ed688a71063
FiodorG <gre_dorelus@yahoo.com>
2021-11-30 14:05:39 -0500

    String to integer in one line

---
+++ One_liners_like_pro.py/str_to_int.py
@@ -0,0 +1,23 @@
+str_1 = '0'
+
+str_2 = '100'
+
+str_3 = '587'
+
+print(type(str_1)) 
+print(str_1)
+print(int(str_1))
+
+print(type(str_2))
+print(str_2) 
+print(int(str_2))
+
+print(type(str_3)) 
+print(str_3)
+print(int(str_3))
+
+
+
+
+
+
