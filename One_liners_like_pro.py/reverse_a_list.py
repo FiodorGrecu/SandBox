67805888c223252024f4a31621e248480933b73b
FiodorG <gre_dorelus@yahoo.com>
2021-12-08 14:26:29 -0500

    Reversing one list inn one line

---
+++ One_liners_like_pro.py/reverse_a_list.py
@@ -0,0 +1,13 @@
+lst = [1,2,3,4,5,6]
+lst_str = ['a', 'b','c','d']
+
+print(lst)
+
+reverse_lst = lst[::-1]
+
+print(reverse_lst)
+
+reverse_lst_1 = list(reversed(lst))
+
+print(reverse_lst_1)
+print(list(reversed(lst_str))) #also an option directly in the print statement one liner
