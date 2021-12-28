db0cbb96381ee19a659fad4d41d9e030c91f8e9b
FiodorG <gre_dorelus@yahoo.com>
2021-12-28 17:05:14 -0500

    Codiliti assesment for Smoothstack attempt

---
+++ Functions/Technical_interview/Codullity_patholes.py
@@ -0,0 +1,40 @@
+# S = ".X..X"
+
+
+# S = ".X..X"
+# print(S)
+# s = S.split()
+# print(s)
+
+
+# for i in s:
+#     if i == ".":
+#         print(i)
+
+# s = int(s)
+# for i in(s):
+#     if i == ".":
+#         print(i)
+
+
+# split word
+# S = ".X..X"
+# print(S)
+# s = S.split()
+# print(s)
+
+# def split(S):
+#     return [char for char in S]
+# S = ".X..X"
+# S = ".X..XXXX."
+# split_s = S.split()
+# print(split(S))
+# print(S.count("X"))
+# print(S.count("."))
+
+# Counting patherns
+class Solution:
+    def countSegments(self, s: str) -> int: 
+        answer = len(s.split()) 
+        return answer
+countSegment
