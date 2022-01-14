fd6c035d481a073554638d81e2c798ee4b3bc919
FiodorG <gre_dorelus@yahoo.com>
2022-01-14 16:07:21 -0500

    User tool for help

--- Refresher/sumation.py
+++ Refresher/sumation.py
@@ -1,6 +1,17 @@
-a = int(input())
-b = int(input())
+import math
+# a = int(input())
+# b = int(input())
+
+
+from array import array
+
+
 def summation(a,b):
     
     return a + b
-print(summation(a,b))
+print(summation(2,3))
+
+# print(help(array))
+
+# Useful hint when stuck during assesment
+print(help('print'))
