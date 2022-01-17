68723b29ad35a894b39c22fa9e81f760f9a7ccd6
FiodorG <gre_dorelus@yahoo.com>
2022-01-16 21:58:52 -0500

    The NaN handling of the expression

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -1,5 +1,17 @@
-let numbers  = [1, 2, 3, 4];
+let numbers  = [1, 2, 3, 4, NaN];
 
-if(numbers.indexOf(2) !== -1) { 
-    console.log('Array contains value');
+// Old JS script  |
+             //   |
+//                V
+
+// if(numbers.indexOf(2) !== -1) { 
+//     console.log('Array contains value');
+// }
+
+// The new Version ES6 of the same scrip 
+
+// if (numbers.includes(3)) 
+if (numbers.includes(NaN)) {
+    console.log('Array contains value (indexOf())')
+
 }
