77eb453fe776f14f2b38d37b0a788372b482ceca
FiodorG <gre_dorelus@yahoo.com>
2022-01-19 11:45:26 -0500

    Methods of lists

---
+++ Grokking/linked_lists.py
@@ -0,0 +1,44 @@
+import array
+from crypt import methods
+
+letters = ["a","b","c"]
+L1 = [1,2,3,4]
+# print(L1,type(L1))
+matrix = [[1,2], [3,4]]
+ones = [1] * 9
+combined = letters + ones
+# print(ones)
+# print(combined)
+l2 = (1,2,3,4,'car')
+
+numbers = list(range(30))
+
+chars = list("I love coding and reading")
+# print(numbers)
+# print(chars)
+# print(len(chars))
+
+# print(l2,type(l2))
+# print(help(tuple(methods)))
+# print(help(array))
+
+    #  |  Methods:
+    #  |  
+    #  |  append() -- append a new item to the end of the array
+    #  |  buffer_info() -- return information giving the current memory info
+    #  |  byteswap() -- byteswap all the items of the array
+    #  |  count() -- return number of occurrences of an object
+    #  |  extend() -- extend array by appending multiple elements from an iterable
+    #  |  fromfile() -- read items from a file object
+    #  |  fromlist() -- append items from the list
+    #  |  frombytes() -- append items from the string
+    #  |  index() -- return index of first occurrence of an object
+    #  |  insert() -- insert a new item into the array at a provided position
+    #  |  pop() -- remove and return item (default last)
+    #  |  remove() -- remove first occurrence of an object
+    #  |  reverse() -- reverse the order of the items in the array
+    #  |  tofile() -- write all items to a file object
+    #  |  tolist() -- return the array converted to an ordinary list
+    #  |  tobytes() -- return the array converted to a string
+    #  |  
+# print(help('typecode'))
