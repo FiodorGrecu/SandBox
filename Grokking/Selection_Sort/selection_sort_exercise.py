4db6b208207cec064a0a3db82c56f80cd4eb563d
FiodorG <gre_dorelus@yahoo.com>
2022-01-25 11:54:58 -0500

    Very good program to sort an array

---
+++ Grokking/Selection_Sort/selection_sort_exercise.py
@@ -0,0 +1,20 @@
+# a function to find the smalles element ain an array.     
+
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1,len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def sellectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(sellectionSort([8,5,8,9,3]))
+print(sellectionSort([-8,5,8,9,3]))
