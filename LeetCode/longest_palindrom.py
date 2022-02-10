49accd3ed44b3bd7518e2a10ec58dfcaf31cc3ba
FiodorG <gre_dorelus@yahoo.com>
2022-02-10 15:28:15 -0500

    The longets palindrome solution the easy to understand version but O(n)

---
+++ LeetCode/longest_palindrom.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        count = 0
+        dict = {}
+        for i in s:
+            dict[i] = dict.get(i, 0) +  1
+            if dict[i] == 2:
+                count += 2
+                del dict[i]
+        if len(dict):
+            count += 1
+        return count
+    
+palindrome = Solution()
+
+print(palindrome.longestPalindrome("abccccdd"))
+print(palindrome.longestPalindrome("eye"))
+print(palindrome.longestPalindrome("civicor"))
+
