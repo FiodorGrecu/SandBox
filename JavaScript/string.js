52b84a0909c8921fb9f42573778f8ebf914a684d
FiodorG <gre_dorelus@yahoo.com>
2022-02-17 22:57:15 -0500

    String repeating 10 times with built in REPEAT function

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -7,4 +7,6 @@
 var myGreetings = 'Sup';
 var myName = 'Dorel';
 var sayGreetings = myGreetings + ',' + myName + '!' //The greeting itself
+
 console.log(sayGreetings)
+console.log(myName.repeat(10))
