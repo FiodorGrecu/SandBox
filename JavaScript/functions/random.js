bf036fb2f9a8af4221f6934ff37f5115b5593851
FiodorG <gre_dorelus@yahoo.com>
2022-02-20 09:09:30 -0500

    Random function in javascript

---
+++ JavaScript/functions/random.js
@@ -0,0 +1,7 @@
+Math.random()
+var list1 = [1,2,3,4,5];
+for (let i = 0; i< list1.lenght; i++) {
+    console.log(Math.random(list1[i]));
+    console.log(list1[i]);
+}
+// console.log(Math.floor(Math.random()*10))
