83ae4597f3fcd8c9102dc45da8ee05edd29da8b7
FiodorG <gre_dorelus@yahoo.com>
2022-02-27 08:40:16 -0500

    Some more improvements/opt/anaconda3/envs/jlab-test/bin/python "/Users/Work/Desktop/Python Files/Practical programing/SandBox/SandBox/Grokking/BubbleSort/bubble_sort.py"

--- Grokking/BubbleSort/bubble_sort.py
+++ Grokking/BubbleSort/bubble_sort.py
@@ -2,11 +2,13 @@
 def bubble_sort(elements):
     size = len(elements)
 
-    for i in range(size-1):
-        if elements[i] > elements[i+1]:
-            tmp = elements[i]
-            elements[i] = elements[i+1]
-            elements[i+1] = tmp
+    # for k in range(2): #this line iterates through last 2 elements elements
+    for k in range(size-1): #this line iterates through all elements
+        for i in range(size - 1 - k): # the minus 'k' here ignores the elementsto the right of the swaped 2 element to save the running time each time we ares swaping the items
+            if elements[i] > elements[i+1]:
+                tmp = elements[i]
+                elements[i] = elements[i+1]
+                elements[i+1] = tmp
 
 if __name__ == '__main__':
 
