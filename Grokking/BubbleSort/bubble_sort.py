a80a286baa125174dfdbc48a2bd2519f20c62d10
FiodorG <gre_dorelus@yahoo.com>
2022-02-27 08:30:34 -0500

    After a litle refactoring and one bug fix it worked

--- Grokking/BubbleSort/bubble_sort.py
+++ Grokking/BubbleSort/bubble_sort.py
@@ -1,14 +1,15 @@
+
 def bubble_sort(elements):
     size = len(elements)
 
     for i in range(size-1):
-        if elements[1] > elements[i+1]:
+        if elements[i] > elements[i+1]:
             tmp = elements[i]
             elements[i] = elements[i+1]
-            elements[i+1] =  tmp
+            elements[i+1] = tmp
 
-elements =[5,8,14,2,25,1,28,55,41]
+if __name__ == '__main__':
 
-
-print(bubble_sort(elements))
-# print(elements)
+    elements = [5,8,14,2,25,1,28,55,41]
+    bubble_sort(elements)
+    print(elements)
