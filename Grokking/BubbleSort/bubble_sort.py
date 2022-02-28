177b8150f26a6a111e0087e7e675f203acc9d790
FiodorG <gre_dorelus@yahoo.com>
2022-02-28 12:39:24 -0500

    Clarification on the bubble sort example in the outer for loop

--- Grokking/BubbleSort/bubble_sort.py
+++ Grokking/BubbleSort/bubble_sort.py
@@ -2,7 +2,7 @@
 def bubble_sort(elements):
     size = len(elements)
 
-    # for k in range(2): #this line iterates through last 2 elements elements
+    # for k in range(2): #this line iterates through all elements and swaps last from the right 2 elements.
     for k in range(size-1): #this line iterates through all elements
         for i in range(size - 1 - k): # the minus 'k' here ignores the elementsto the right of the swaped 2 element to save the running time each time we ares swaping the items
             if elements[i] > elements[i+1]:
