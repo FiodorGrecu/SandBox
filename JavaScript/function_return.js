a40d23da2f5680310df9426648c63e2a86de735c
FiodorG <gre_dorelus@yahoo.com>
2022-03-03 22:21:54 -0500

    function return refresher exercise

---
+++ JavaScript/function_return.js
@@ -0,0 +1,23 @@
+var add = function(num1, num2){
+    return num1 + num2
+}
+
+console.log(add(5, 23))
+
+var subtract = function(num1, num2){
+    return num1 - num2
+}
+
+console.log(subtract(5, 3)) 
+
+var multiply = function(num1, num2){
+    return num1 * num2
+}
+
+console.log(multiply(5 ,2))
+
+var div = function(num1, num2){
+    return num1 / num2
+}
+
+console.log(div(24, 2))
