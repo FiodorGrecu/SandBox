480a1983000bdca734487287f1497d331a61e29d
FiodorG <gre_dorelus@yahoo.com>
2022-03-03 22:44:25 -0500

    moved 2 files to functions dir

---
+++ JavaScript/variables/variables.js/global_variables.js
@@ -0,0 +1,11 @@
+var lifeInches = 37;
+
+var calcInches = function(startAge, endAge, inchesPerYear) {
+    var totalInches = inchesPerYear * (endAge - startAge)
+    lifeInches += totalInches;
+    return totalInches;
+}
+
+console.log("From 0 - 7 years:" + calcInches(0,7,2))
+console.log("From 7 - 16 years:" + calcInches(7,16, 4))
+console.log("Total growth over life :" + lifeInches)
