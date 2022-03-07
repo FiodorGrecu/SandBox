0fd0e9a4f43cfbb33aef142504c3b4f27ff0dea8
FiodorG <gre_dorelus@yahoo.com>
2022-03-06 19:52:08 -0500

    Factorial testing

--- One_liners_like_pro.py/find_factorial.py
+++ One_liners_like_pro.py/find_factorial.py
@@ -7,3 +7,5 @@ factorial = lambda num : 1 if num <= 1 else num*factorial(num-1)
 
 print('Factorial of', 'num1',':',factorial(num1))
 print('Factorial of', 'num2',':',factorial(num2))
+print('Factorial of', 'num3',':',factorial(num3))
+print('Factorial of', 'num4',':',factorial(num4))
