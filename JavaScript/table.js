d8de76ea9e50d8ed06823b7423b261644ba041d6
FiodorG <gre_dorelus@yahoo.com>
2022-03-21 17:07:58 -0400

    table library test with multipe columns

--- JavaScript/table.js
+++ JavaScript/table.js
@@ -1,10 +1,11 @@
-const people = [
+const people_db = [
 
-    {name: "Dorel", age: 37},
-    {name: "Sebastian", age: 7},
-    {name: "Vitalii", age: 42},
+    {name: "Dorel", age: 37, zodiac: 'gemini', address: "Reston"},
+    {name: "Sebastian", age: 7, zodiac: 'scorpio', address: "Reston"},
+    {name: "Vitalii", age: 42, zodiac: 'Virgo', address: "Milan"},
+    {name: "Natalia", age: 37, zodiac: 'Leo', address: "Reston"},
 
 ];
 
-console.table(people)
+console.table(people_db)
 // console.log(people)
