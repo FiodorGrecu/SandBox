919e800ae43bd8a78e561425274bef167cd5ddd4
FiodorG <gre_dorelus@yahoo.com>
2022-03-23 18:08:28 -0400

    Adding more columns to the table

--- JavaScript/table/array_of_objects.js
+++ JavaScript/table/array_of_objects.js
@@ -1,13 +1,14 @@
-function Person(firstName, lastName, age, zodiac) {
+function Person(firstName, lastName, age, zodiac, car) {
     this.firstName = firstName;
     this.lasttName = lastName;
     this.age = age;
     this.zodiac = zodiac;
+    this.car = car;
 }
 
-var me = new Person("Fiodor", "Grecu", 37, "Gemini");
-var natalia = new Person("Natalia", "Grecu", 37, "Leo");
-var sebastian = new Person("Sebastian", "Grecu", 6, "Scorpio");
+var me = new Person("Fiodor", "Grecu", 37, "Gemini", "Jeep");
+var natalia = new Person("Natalia", "Grecu", 37, "Leo", "Mercedes");
+var sebastian = new Person("Sebastian", "Grecu", 6, "Scorpio", "None");
 
 // console.table([me, natalia, sebastian], ["firstName"]["lastName"]["age"]["zodiac"]);
 console.table([me, natalia, sebastian],["firstName"]['lastName']);
