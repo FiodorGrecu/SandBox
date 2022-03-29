9c0ad0030d80ce53f3c93f3c35ed2eeef4e0ec38
FiodorG <gre_dorelus@yahoo.com>
2022-03-28 20:21:05 -0400

    Java is confusing

---
+++ Java/self_paced.java
@@ -0,0 +1,9 @@
+package Java;
+
+public class self_paced {
+    public static void main(String[] args) {
+        System.out.println("Truying again");
+        System.out.println("Not Bad");
+    }
+    
+}
