7eb749011ce1e77f3fb954a10f1d7996c4df54d6
FiodorG <gre_dorelus@yahoo.com>
2022-03-28 20:37:20 -0400

    Data types in java

--- Java/self_paced.java
+++ Java/self_paced.java
@@ -2,8 +2,19 @@ package Java;
 
 public class self_paced {
     public static void main(String[] args) {
-        System.out.println("Truying again");
-        System.out.println("Not Bad");
+       int number = 5;
+       double number2 = 5.0;
+       boolean b = true;
+       char c = 'h';
+       String str = "toy";
+
+       
+
+       System.out.println(number);
+       System.out.println(number2);
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(str);
     }
     
 }
