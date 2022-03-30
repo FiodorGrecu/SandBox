c3b2a2d507476e949a94b2f61a7dfcce5c02d710
FiodorG <gre_dorelus@yahoo.com>
2022-03-30 17:10:29 -0400

    Basic division added to the basic operations

--- Java/basicOperations.java
+++ Java/basicOperations.java
@@ -5,9 +5,14 @@ public class basicOperations {
         int a = 5;
         int b = 3;
         int c = 6;
-        int sum = a + b + c;
+        // int sum = a * b * c;
+        // int div = a / b; // we have a interesting case for division.
 
+        // double is refered to float in Java
+        // double div = a / b;
+        double div = 7 / b;
 
-        System.out.println(sum);
+        // System.out.println(sum);
+        System.out.println(div);
     }
 }
