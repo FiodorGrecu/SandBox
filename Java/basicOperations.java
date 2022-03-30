dfc51a3bf17411dc09f8757730f354aec88e99f2
FiodorG <gre_dorelus@yahoo.com>
2022-03-30 16:58:27 -0400

    Basic math in Java

---
+++ Java/basicOperations.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class basicOperations {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = 6;
+        int sum = a + b + c;
+
+
+        System.out.println(sum);
+    }
+}
