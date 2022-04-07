3d859125c4720d9de5b0e2aa344afd29b344b2cd
FiodorG <gre_dorelus@yahoo.com>
2022-04-07 13:40:27 -0400

    Exponent example with assignment

--- Java/Exponents.java
+++ Java/Exponents.java
@@ -1,9 +1,13 @@
-
 public class Exponents {
     public static void main(String[] args){
 
+        int a = 5;
+        int b = 2;
+        double exp = Math.pow(a, b);
+
         System.out.println(Math.pow(5,3));
         System.out.println(Math.pow(5,5));
+        System.out.println(exp);
 
     }
 }
