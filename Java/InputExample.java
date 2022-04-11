bd95548709e7cee1352c72b9932f9fa35f91a71e
FiodorG <gre_dorelus@yahoo.com>
2022-04-10 22:52:07 -0400

    Java stil not working for me in the testing fase

--- Java/InputExample.java
+++ Java/InputExample.java
@@ -9,18 +9,20 @@ public class InputExample {
         int age;
         float shoeSize;
 
-        System.out.println("Please enter name:");
-        name = scanner.nextLine();
+        System.out.println(scanner);
 
-        System.out.println("Please enter your age:");
-        age = scanner.nextInt();
+        // System.out.println("Please enter name:");
+        // name = scanner.nextLine();
 
-        System.out.println("Please enter you shoe size:");
-        shoeSize = scanner.nextFloat();
+        // System.out.println("Please enter your age:");
+        // age = scanner.nextInt();
 
-        // System.out.println("Your name is:  " + name);
-        System.out.println("Your age is:  " + age);
-        System.out.println("Your shoeSize is:  " + shoeSize);
+        // System.out.println("Please enter you shoe size:");
+        // shoeSize = scanner.nextFloat();
+
+        // // System.out.println("Your name is:  " + name);
+        // System.out.println("Your age is:  " + age);
+        // System.out.println("Your shoeSize is:  " + shoeSize);
 
 
 
