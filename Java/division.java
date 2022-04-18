d9605a2c732399d9d0785f1cca4410f41fea15e1
FiodorG <gre_dorelus@yahoo.com>
2022-04-18 19:02:21 -0400

    Java exercises

--- Java/division.java
+++ Java/division.java
@@ -4,11 +4,11 @@ public class division {
     public static void main(String[] args) {
         int a = 4;
         int b = 3;
-        // double div = a / b;
+        double div = a / b;
         //now we have multiple digit after the coma
         // double expon = Math.pow(b, a);
-        double num = b / a;
+        // double num = b / a;
 
-        System.out.println(num); 
+        System.out.println(div); 
     }
 }
