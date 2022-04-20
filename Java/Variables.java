3c8d598d53da8d767edbd7ea98ba3fe78aa5c093
FiodorG <gre_dorelus@yahoo.com>
2022-04-20 18:21:28 -0400

    Working with variables in Java

---
+++ Java/Variables.java
@@ -0,0 +1,15 @@
+/**
+ * Variables
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y = 7;
+        // double z = 0.5;
+        double total = x + y;
+
+        System.out.println(total);
+    }
+}
