9ce3b871248571ddc386e643cf5b686b103d9664
FiodorG <gre_dorelus@yahoo.com>
2022-04-25 17:52:41 -0400

    If statement in Java

---
+++ Java/NoorTeck/DayOne/src/IfStatementDemo.java
@@ -0,0 +1,15 @@
+/**
+ * IfStatementDemo
+ */
+public class IfStatementDemo {
+
+    public static void main(String[] args) {
+        int numOne = 10;
+        int numTwo = 12;
+
+        if (numOne > numTwo){
+            System.out.println("NumOne is greater then NumTwo");
+        }
+        System.out.println("Outside of the loop...");
+    }
+}
