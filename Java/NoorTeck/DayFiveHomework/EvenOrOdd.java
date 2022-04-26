7116f7e625a98927e76258b5bbb9e3663f6550e7
FiodorG <gre_dorelus@yahoo.com>
2022-04-26 16:28:39 -0400

    Even or Odd exercise

---
+++ Java/NoorTeck/DayFiveHomework/EvenOrOdd.java
@@ -0,0 +1,18 @@
+/**
+ * EvenOrOdd
+ */
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+        
+        int number;
+        number = 4;
+
+        if(number % 2 == 0){
+            System.out.println("EVEN...");
+        } else {
+            System.out.println("ODD...");
+        }
+
+    }
+}
