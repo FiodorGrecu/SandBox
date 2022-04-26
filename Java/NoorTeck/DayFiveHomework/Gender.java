9b1a55fecf83c8e32169ca6846276580ad0b9435
FiodorG <gre_dorelus@yahoo.com>
2022-04-26 17:13:49 -0400

    Gender exercise in Java

---
+++ Java/NoorTeck/DayFiveHomework/Gender.java
@@ -0,0 +1,22 @@
+/**
+ * Gender
+ */
+public class Gender {
+    public static void main(String[] args) {
+        char gender = 't';
+
+        if (gender == 'm') {
+            System.out.println("Male");
+        } else if (gender == 'M') {
+            System.out.println("Male");
+        } else if(gender == 'f') {
+            System.out.println("Female");
+        } else if(gender == 'F') {
+            System.out.println("Female");
+        }else {
+            System.out.println("others");
+        }
+        
+    }
+     
+}
