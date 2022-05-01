0da38e019bc0da1493a38cef44334b6c4f95d329
FiodorG <gre_dorelus@yahoo.com>
2022-05-01 14:01:24 -0400

    While Loop Coza Loza Woza exercise

---
+++ Java/NoorTeck/DaySeven/WhileLoopCozaLozaWoza.java
@@ -0,0 +1,30 @@
+public class WhileLoopCozaLozaWoza {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 36) {
+            
+            if ( i % 3 == 0 && i % 5 == 0) {
+                System.out.println("CozaLoza");
+
+            } else if ( i % 3 == 0 ) {
+                System.out.println("Coza");
+
+            } else if ( i % 5 == 0 ) {
+                System.out.println("Loza");
+
+            } else if ( i % 7 == 0 ) {
+                System.out.println("Woza");
+
+            } else {
+                System.out.println(i);
+            }
+
+            i++;
+
+        }
+
+    }
+}
