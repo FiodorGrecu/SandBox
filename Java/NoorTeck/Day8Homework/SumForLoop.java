8ae47ed872a94f7a8479e43410dad7eb703f4151
FiodorG <gre_dorelus@yahoo.com>
2022-05-02 18:15:10 -0400

    Sum of a for loop in Java

---
+++ Java/NoorTeck/Day8Homework/SumForLoop.java
@@ -0,0 +1,12 @@
+public class SumForLoop {
+    public static void main(String[] args) {
+        
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        for ( int i = 1; i <= 20; i++){
+            
+        }
+
+    }
+}
