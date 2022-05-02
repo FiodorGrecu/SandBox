fdf261199728e53da3a7eb26bbfe7c31f5b87272
FiodorG <gre_dorelus@yahoo.com>
2022-05-02 18:41:10 -0400

    Sum For loop exercise in Java

--- Java/NoorTeck/Day8Homework/SumForLoop.java
+++ Java/NoorTeck/Day8Homework/SumForLoop.java
@@ -5,8 +5,16 @@ public class SumForLoop {
         int sumOdd = 0;
 
         for ( int i = 1; i <= 20; i++){
-            
+
+            if (i % 2 == 0 ) {
+                sumEven += i;
+            } else {
+                sumOdd += i;
+            }
+
         }
+        System.out.println("Sum of EVEN Numbers: " + sumEven);
+		System.out.println("Sum of ODD Numbers:  " + sumOdd);
 
     }
 }
