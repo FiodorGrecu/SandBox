568947d5692c29252be8f710baca051f3b960457
FiodorG <gre_dorelus@yahoo.com>
2022-05-03 17:45:14 -0400

    For loop with explanation of flow control

---
+++ Java/NoorTeck/Day6/ForLoopDemo.java
@@ -0,0 +1,14 @@
+public class ForLoopDemo {
+
+    public static void main(String[] args) {
+        
+        // looping through numbers 1-5
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println(i);
+        }
+
+    }
+    
+}
