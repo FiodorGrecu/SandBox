b8fd7b80285df7f84d16a45a5379bb3dee44017c
FiodorG <gre_dorelus@yahoo.com>
2022-05-04 22:23:22 -0400

    Methods body and logic

---
+++ Java/NoorTeck/Day9/MethodDemo.java
@@ -0,0 +1,19 @@
+public class MethodDemo {
+
+    public static void getDayOfTheWeek(int number) {
+
+        if(number==1){
+            System.out.println("Mondat");
+        } else if (number == 2){
+            System.out.println("Tuesday");
+        } else if (number == 3){
+            System.out.println("Wednsday");
+        }else if (number == 4){
+            System.out.println("Thursday");
+        }
+
+
+
+    }
+
+}
