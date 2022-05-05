b9944812ef22fea8906b186097c97b81428e9c56
FiodorG <gre_dorelus@yahoo.com>
2022-05-05 17:25:59 -0400

    Greetings function

---
+++ Java/NoorTeck/Day9/ExerciseOneGreetings.java
@@ -0,0 +1,12 @@
+public class ExerciseOneGreetings {
+    
+    public static void main(String[] args) {
+        greetings("Fiodor");
+        greetings("Sebastian");
+    }
+
+    public static void greetings(String name){
+
+        System.out.println("Hello, my name is: " + name);
+    }
+}
