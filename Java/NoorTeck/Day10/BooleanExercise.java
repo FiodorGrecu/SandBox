92403c3531114338da983ceb350cabc45c08d367
FiodorG <gre_dorelus@yahoo.com>
2022-05-08 13:33:44 -0400

    Sleep Status exercise

---
+++ Java/NoorTeck/Day10/BooleanExercise.java
@@ -0,0 +1,25 @@
+public class BooleanExercise {
+    
+    public static void main(String[] args) {
+		
+		boolean amISleeping = sleepIn(false, false);
+		System.out.println(amISleeping);
+//		boolean amISleeping2 = sleepIn(true, true);
+//		System.out.println(amISleeping2);
+			
+	}
+
+	public static boolean sleepIn(boolean isWeekday, boolean isVacation) {
+		
+		boolean result = false;
+		
+		if(isWeekday || isVacation) {
+			result = false;
+		}else {
+			result = true;
+		}
+		
+		return result;
+	}
+
+}
