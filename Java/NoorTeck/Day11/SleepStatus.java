d9c40ed56b181363f2a134acb25684dab3d74cf9
FiodorG <gre_dorelus@yahoo.com>
2022-05-09 11:46:44 -0400

    Added the problem requirements

--- Java/NoorTeck/Day11/SleepStatus.java
+++ Java/NoorTeck/Day11/SleepStatus.java
@@ -6,8 +6,8 @@ public class SleepStatus {
         System.out.println(amISleeping);
 
     }
-        //  VERSION 1
-        
+    // VERSION 1
+
     public static boolean sleepIn(boolean isWeekday, boolean isVacation) {
         boolean result = false;
 
@@ -22,3 +22,20 @@ public class SleepStatus {
     }
 
 }
+/**
+                 INSTRUCTIONS OF THE PROBLEM:
+
+                Write a method that takes Boolean parameters "isWeekday" and "isVacation".
+                
+                The parameter "isWeekday" is true if it is a weekday, the parameter "isVacation"
+                is true if we are on vacation. We sleep in if it is not a weekday otherwise return false.
+                
+                Then call your function from main method: Create a Boolean variable in your main method and 
+                assign the value what "sleepIn" method returns 
+                Variable Name: Boolean amIsleeping;
+                
+                Example:
+                if pass false, false value then your method should return true
+                sleepIn (false, false) → true
+            
+ */
