3f732d6bb10a763e2d1970ee5178a6bb03cb8cd8
FiodorG <gre_dorelus@yahoo.com>
2022-05-12 14:41:11 -0400

    Instance variable access expalnation

---
+++ Java/NoorTeck/Day13/Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+    
+    int age;
+    String country;
+
+    public static void main(String [] args){
+
+        // local variables
+        String name = "John";
+        System.out.println(name);
+
+    }
+
+}
+/**
+ *      INSTANCE VARIABLE  --> declared inside the CLASS but OUTSIDE the body.
+ * 
+ *      LOCAL VARIABLE     --> declared inside the METHOD BODY.
+ * 
+ * */
