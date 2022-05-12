22ef7e9fc5a5b0ea8adc3bf901b5a0f188854c41
FiodorG <gre_dorelus@yahoo.com>
2022-05-12 16:05:13 -0400

    Instance variables and 3 Objects that are associated with them

---
+++ Java/NoorTeck/Day13/Person.java
@@ -0,0 +1,31 @@
+import javax.print.attribute.standard.PrinterStateReasons;
+
+public class Person {
+    
+    int id;
+    double salary;
+
+    public static void main(String[] args){
+
+        Person p1 = new Person();
+        p1.id = 1;
+        p1.salary = 11.11;
+
+        Person p2 = new Person();
+        p2.id = 2;
+        p2.salary = 22.22;
+
+        Person p3 = new Person();
+        p3.id = 3;
+        p3.salary = 33.33;
+
+        System.out.println(p1.id);
+        System.out.println(p1.salary);
+        System.out.println(p2.id);
+        System.out.println(p2.salary);
+        System.out.println(p3.id);
+        System.out.println(p3.salary);
+
+    }
+
+}
