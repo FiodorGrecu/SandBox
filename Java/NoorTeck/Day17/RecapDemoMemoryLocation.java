576d3739e39d31f9356d3b6e541ff2ed68330c39
FiodorG <gre_dorelus@yahoo.com>
2022-05-19 13:11:59 -0400

    Checking the equality of similar an non similar string values

--- Java/NoorTeck/Day17/RecapDemoMemoryLocation.java
+++ Java/NoorTeck/Day17/RecapDemoMemoryLocation.java
@@ -1,13 +1,13 @@
 public class RecapDemoMemoryLocation {
-    
+
     public static void main(String[] args) {
-        
+
         int numOne = 10;
         int numTwo = 20;
 
-        if(numOne == numTwo){
+        if (numOne == numTwo) {
             System.out.println("Equal");
-        }else{
+        } else {
             System.out.println("Not Eqaul"); // this is going to print
         }
 
@@ -16,10 +16,32 @@ public class RecapDemoMemoryLocation {
         String s1 = "John";
         String s2 = "John";
 
-        if(s1 == s2){
+        if (s1 == s2) {
+            System.out.println("Equal"); // this is going to print
+        } else {
+            System.out.println("Not Eqaul");
+        }
+
+        System.out.println("***************");
+
+        String n1 = new String("Fiodor");
+        String n2 = new String("Fiodor");
+
+        if (n1 == n2) {
             System.out.println("Equal");
-        }else{
+        } else {
             System.out.println("Not Eqaul"); // this is going to print
         }
+
+        System.out.println("***************");
+
+        String p1 = "Fiodor";
+        String p2 = new String("Fiodor");
+
+        if (p1 == p2){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");}
+
     }
 }
