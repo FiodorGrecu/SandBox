eee70457bacbe1b5c9d1be50a89e825ad3b52f71
FiodorG <gre_dorelus@yahoo.com>
2022-05-19 11:56:32 -0400

    Checking if the values are equal in the memory location

---
+++ Java/NoorTeck/Day17/RecapDemoMemoryLocation.java
@@ -0,0 +1,14 @@
+public class RecapDemoMemoryLocation {
+    
+    public static void main(String[] args) {
+        
+        int numOne = 10;
+        int numTwo = 20;
+
+        if(numOne == numTwo){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Eqaul");
+        }
+    }
+}
