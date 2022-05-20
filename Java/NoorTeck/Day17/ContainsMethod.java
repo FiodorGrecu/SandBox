4e39cc8a756a7b0b3159a14479f58b63b1476566
FiodorG <gre_dorelus@yahoo.com>
2022-05-20 10:52:48 -0400

    Cought speeding problem from CodingBat in python

---
+++ Java/NoorTeck/Day17/ContainsMethod.java
@@ -0,0 +1,13 @@
+public class ContainsMethod {
+    
+    public static void main(String[] args) {
+        
+
+        String s1 = "I love java because it is powerfull";
+
+        System.out.println(s1.contains("ava"));
+        System.out.println(s1.contains("full"));
+        System.out.println(s1.contains("an"));
+        System.out.println(s1.contains("ox"));
+    }
+}
