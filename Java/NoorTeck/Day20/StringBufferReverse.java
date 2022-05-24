5b9ef45a926b37fd740f27ba731b9284903bb8dd
FiodorG <gre_dorelus@yahoo.com>
2022-05-24 16:36:00 -0400

    SrtingBuffer reverse

---
+++ Java/NoorTeck/Day20/StringBufferReverse.java
@@ -0,0 +1,15 @@
+public class StringBufferReverse {
+    
+    public static void main(String[] args) {
+    
+        StringBuffer sb = new StringBuffer("Dorel");
+
+        sb.reverse();
+        System.out.println(sb);
+
+        
+
+}
+
+
+}
