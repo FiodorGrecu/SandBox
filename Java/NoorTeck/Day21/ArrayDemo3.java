35a9592098f2bb1ead0122336ed76b6776b21965
FiodorG <gre_dorelus@yahoo.com>
2022-05-28 16:18:25 -0400

    Reverse for loop of an array

--- Java/NoorTeck/Day21/ArrayDemo3.java
+++ Java/NoorTeck/Day21/ArrayDemo3.java
@@ -3,9 +3,11 @@ public class ArrayDemo3 {
 public static void main(String[] args) {
     
     int [] nums = {10,20,30,40,50,60,70,80,90,100};
-    
 
-
+    for (int i = nums.length-1; i >= 0; i--){
+        System.out.println(nums[i]);
+    }
+  
 }
 
 
