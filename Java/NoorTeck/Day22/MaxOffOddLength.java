a9b581d4e789bf3d0a535868a9a053d0e66d37bb
FiodorG <gre_dorelus@yahoo.com>
2022-06-01 10:59:30 -0400

    A Probblem that is looing to find the max number in a odd array only by looking at first last and middle elements

---
+++ Java/NoorTeck/Day22/MaxOffOddLength.java
@@ -0,0 +1,29 @@
+public class MaxOffOddLength {
+    
+    public int maxTripple(int [] numbers){
+        int result = numbers[0];
+
+        return result;
+    }
+
+}
+
+
+/**
+ *      Given an array of integers of odd length, look at first , last and middle elements value in the array
+ *       and returnthe largest value(number). The array length will be at least one. 
+ * 
+ *          Test Data:
+ *              maxTripple(1,2,3) --> 3
+ *              maxTripple(5,2,3) --> 5
+ *              maxTripple(5,1,7,3,7,9,8) --> 8
+ * 
+ *                  Access Modifier : public
+ *                  Non - Access Modifier : non-static
+ *                  Return Type : int
+ *                  Method Name : maxTripple 
+ *                  Parameter : int [] numbers
+  * 
+ * 
+ * 
+ */
