c49ee25f890aae459f1fbeb86dbe58dc7abf854b
FiodorG <gre_dorelus@yahoo.com>
2022-06-07 14:45:29 -0400

    Inheritance Person class and extend keyword

---
+++ Java/NoorTeck/Day26Inheritance/StudentDemo.java
@@ -0,0 +1,18 @@
+public class StudentDemo {
+    
+    public static void main(String[] args) {
+        
+        Person p1 = new Person();
+
+        p1.setName("George");
+        p1.setAge(56);
+        p1.setGender('M');
+        System.out.println(p1.getName());
+        System.out.println(p1.getAge());
+        System.out.println(p1.getGender());
+
+
+
+    }
+
+}
