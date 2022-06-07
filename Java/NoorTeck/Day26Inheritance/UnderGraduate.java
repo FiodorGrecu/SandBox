78b73ca6a2a0ffde642c029d8a57ce5b73eb1487
FiodorG <gre_dorelus@yahoo.com>
2022-06-07 16:31:08 -0400

    Created the inheritance Undergraduate Class

--- Java/NoorTeck/Day26Inheritance/UnderGraduate.java
+++ Java/NoorTeck/Day26Inheritance/UnderGraduate.java
@@ -1,3 +1,15 @@
-public class UnderGraduate {
+public class UnderGraduate extends Student {
     
+    private String grade;
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    
+
 }
