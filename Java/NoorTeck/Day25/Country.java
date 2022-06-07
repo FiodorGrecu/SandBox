8c8aed23904f168bcc0b740f9102f225efcf3fb4
FiodorG <gre_dorelus@yahoo.com>
2022-06-06 22:25:35 -0400

    Encapsulation in Java

---
+++ Java/NoorTeck/Day25/Country.java
@@ -0,0 +1,16 @@
+public class Country {
+    // private instatnce
+    private int population;
+
+    // get method
+
+    public int getPopulation() {
+        return population;
+    }
+    // set method
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+
+}
