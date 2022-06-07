985c81b77f9660d783b05867c528e2059ebd179b
FiodorG <gre_dorelus@yahoo.com>
2022-06-07 16:43:46 -0400

    Called the Undergrad class to see what prints after I crweated the Object

--- Java/NoorTeck/Day26Inheritance/StudentDemo.java
+++ Java/NoorTeck/Day26Inheritance/StudentDemo.java
@@ -29,23 +29,6 @@ public class StudentDemo {
         System.out.println(p1.getAge());
         System.out.println(p1.getGender());
 
-        System.out.println("******* UNDERGRAD CLASS ****");
-
-        UnderGraduate u1 = new UnderGraduate();
-
-        u1.setName("John");
-        u1.setAge(18);
-        u1.setGrade(12);
-        u1.setId(112);
-        u1.setGender('M');
-        u1.setMajor("HighSchool");
-
-        System.out.println(u1.getName());
-        System.out.println(u1.getAge());
-        System.out.println(u1.getGender());
-        System.out.println(u1.getGrade());
-        System.out.println(u1.getId());
-        System.out.println(u1.getMajor());
 
     }
 
--- Java/NoorTeck/Day26Inheritance/UndergraduateDemo.java
+++ Java/NoorTeck/Day26Inheritance/UndergraduateDemo.java
@@ -1,5 +1,25 @@
 public class UndergraduateDemo {
     
-    
+    public static void main(String[] args) {
+        
+
+        UnderGraduate u1 = new UnderGraduate();
+
+        System.out.println("******* UNDERGRAD CLASS ****");
+
+        u1.setName("John");
+        u1.setAge(18);
+        u1.setGrade(12);
+        u1.setId(112);
+        u1.setGender('M');
+        u1.setMajor("HighSchool");
+
+        System.out.println(u1.getName());
+        System.out.println(u1.getAge());
+        System.out.println(u1.getGender());
+        System.out.println(u1.getGrade());
+        System.out.println(u1.getId());
+        System.out.println(u1.getMajor());
+    }
 
 }
