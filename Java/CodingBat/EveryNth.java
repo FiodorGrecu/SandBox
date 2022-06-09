7161289784d36c5688d6666c1caf953a6a5a02ed
FiodorG <gre_dorelus@yahoo.com>
2022-06-09 16:30:56 -0400

    Every Nth index in aa string

--- Java/CodingBat/EveryNth.java
+++ Java/CodingBat/EveryNth.java
@@ -4,7 +4,9 @@ public class EveryNth {
         EveryNth obj = new EveryNth();
 
         String strNth = obj.everyNth("Miracle", 2);
+        String strNth1 = obj.everyNth("Oracle", 3);
         System.out.println(strNth);
+        System.out.println(strNth1);
 
     }
     public String everyNth(String str, int n){
@@ -15,6 +17,19 @@ public class EveryNth {
             result += arr[i];
         }
         return result;
+        // return result.toUpperCase();
     }
     
 }
+/**
+ *          
+            Given a non-empty string and an int N, return the string made starting with char 0, 
+            and then every Nth char of the string. So if N is 3, 
+            use char 0, 3, 6, ... and so on. N is 1 or more.
+
+                everyNth("Miracle", 2) → "Mrce"
+                everyNth("abcdefg", 2) → "aceg"
+                everyNth("abcdefg", 3) → "adg"
+
+ * 
+ */
