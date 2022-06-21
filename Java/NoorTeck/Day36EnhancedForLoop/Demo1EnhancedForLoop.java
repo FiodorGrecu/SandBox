28be6d88f551c4f1f232150cd1e4d6b41ca87a74
FiodorG <gre_dorelus@yahoo.com>
2022-06-20 22:55:40 -0400

    Moved the folder to new location

--- Java/NoorTeck/Day36EnhancedForLoop/Demo1EnhancedForLoop.java
+++ Java/NoorTeck/Day36EnhancedForLoop/Demo1EnhancedForLoop.java
@@ -5,7 +5,7 @@ public class Demo1EnhancedForLoop {
     public static void main(String[] args) {
         String country [] = {"USA", "Canada", "Brazil", "Moldova"};
 
-        for (var c : country) {
+        for (String c : country) {
             System.out.println(c);
         }
     }
