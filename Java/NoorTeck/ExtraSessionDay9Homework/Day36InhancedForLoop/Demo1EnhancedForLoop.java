d07930c8db77c348e283d013a269f657926d8965
FiodorG <gre_dorelus@yahoo.com>
2022-06-20 22:53:36 -0400

    Example EnhancedFor Loop

---
+++ Java/NoorTeck/ExtraSessionDay9Homework/Day36InhancedForLoop/Demo1EnhancedForLoop.java
@@ -0,0 +1,12 @@
+
+public class Demo1EnhancedForLoop {
+    
+
+    public static void main(String[] args) {
+        String country [] = {"USA", "Canada", "Brazil", "Moldova"};
+
+        for (var c : country) {
+            System.out.println(c);
+        }
+    }
+}
