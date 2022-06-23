ddc7e4c9550eef764acf3b6858aecd2c4511e244
FiodorG <gre_dorelus@yahoo.com>
2022-06-23 16:21:58 -0400

    Math abs does not work

---
+++ Java/NoorTeck/Java Math class/MathAbs.java
@@ -0,0 +1,16 @@
+public class MathAbs {
+    // import java lang MathAbs;
+    /**
+            In this example I will show the absolute value of a number 
+     
+     */
+
+     public static void main(String[] args) {
+        
+        int x = 78;  
+        System.out.println(Math.abs(x));  
+     }
+
+    
+
+}
