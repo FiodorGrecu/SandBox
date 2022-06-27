3afef4b206adeaab1286dde58bfd9d4b2033d7e4
FiodorG <gre_dorelus@yahoo.com>
2022-06-27 13:13:26 -0400

    Not able to perform  a enhanced for loop on a String

--- Java/CodingBat/Practice.java
+++ Java/CodingBat/Practice.java
@@ -1,15 +1,24 @@
 public class Practice {
     public static void main(String[] args) {
 
-        int [] nums = {1,2,4,6};
-        int count = 0;
-        for (var n : nums) {
-            if(n%2 == 0){
-                count++;  
-            }
+        // Simple counter program
+
+        // int [] nums = {1,2,4,6};
+        // int count = 0;
+        // for (var n : nums) {
+        //     if(n%2 == 0){
+        //         count++;  
+        //     }
             
-        }
-        System.out.println(count);
+        // }
+        // System.out.println(count);
+
+        // String name = "dorel";
+        // char [] newName = name.toCharArray();  
+        // for (String n : newName) {              So yes not possible to implement a 
+                                                    // enhanced for loop to a string
+            
+        // }
     }
    
     
