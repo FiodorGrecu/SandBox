f1161f511845da407f4cb116ba758d8e204a0105
FiodorG <gre_dorelus@yahoo.com>
2022-07-05 22:01:33 -0400

    MAp work

---
+++ Java/NoorTeck/ExtraSession/Day21/MapDemo1.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo1 {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> mapColor = new HashMap<>();
+
+    }
+}
