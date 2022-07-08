8fffa843e15dfff9cbd670207d72161e804c9ed0
FiodorG <gre_dorelus@yahoo.com>
2022-07-07 21:49:41 -0400

    Contination of the technical interview questions Prob # 11

---
+++ Java/NoorTeck/ZCoding interview/IsAnagram.java
@@ -0,0 +1,17 @@
+public class IsAnagram {
+    /**
+     * 11. Write a java program to check whether two strings are anagram or not?
+     * a. If two strings contain same set of characters but in different order, then
+     * the two
+     * strings are called anagrams
+     */
+
+    // Step 1. Creating a method with boolean return type that takes 2 String
+    // parameters
+    public static boolean isAnagrams(String wordA, String wordB) {
+        // Step 2. Creating a variable named result and initializing it to false
+        boolean result = false;
+
+        return result;
+    }
+}
