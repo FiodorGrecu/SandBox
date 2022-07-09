64c1f5de40777c11b8e28e13767acde435f58e3f
FiodorG <gre_dorelus@yahoo.com>
2022-07-09 14:52:50 -0400

    Within 2 close to 10 Problem

--- Java/NoorTeck/ZCoding interview/IsAnagram.java
+++ Java/NoorTeck/ZCoding interview/IsAnagram.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class IsAnagram {
     /**
      * 11. Write a java program to check whether two strings are anagram or not?
@@ -5,6 +7,13 @@ public class IsAnagram {
      * the two
      * strings are called anagrams
      */
+
+    public static void main(String[] args) {
+
+        String str1 = "listen";
+        String str2 = "silent";
+        isAnagrams(str1, str2);
+    }
 
     // Step 1. Creating a method with boolean return type that takes 2 String
     // parameters
@@ -19,6 +28,17 @@ public class IsAnagram {
             // Step 4. Convert both string to char array using toCharArray() built in method
             char[] arrWordA = wordA.toCharArray();
             char[] arrWordB = wordB.toCharArray();
+
+            // One approach is to sort the 2 arrays and then compare them to each other
+            // but this is going to just visually make them equal.
+            // in the background they will be stored diferently and the values of
+            // the 2 arrays is going to be different so will result to false.
+
+            Arrays.sort(arrWordA);
+            Arrays.sort(arrWordB);
+            System.out.println(arrWordA); // eilnst
+            System.out.println(arrWordB); // eilnst
+            System.out.println(arrWordA.equals(arrWordB));
 
         }
 
