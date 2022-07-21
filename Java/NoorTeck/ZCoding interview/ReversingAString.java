13ca5dc2ab2abaca892edec75f113321d67359c0
FiodorG <gre_dorelus@yahoo.com>
2022-07-21 12:14:15 -0400

    Revers string using chararray and using a reverse for loop

--- Java/NoorTeck/ZCoding interview/ReversingAString.java
+++ Java/NoorTeck/ZCoding interview/ReversingAString.java
@@ -1,15 +1,16 @@
 public class ReversingAString {
 
     public static void main(String[] args) {
-
+        String result = "Java";
+        reverseString(result);
     }
 
     public static String reverseString(String str) {
         String result = "";
 
         char[] strArr = str.toCharArray();
-        for (int i = 0; i < strArr.length; i++) {
-
+        for (int i = strArr.length - 1; i >= 0; i--) {
+            System.out.println(strArr[i]);
         }
 
         return result;
