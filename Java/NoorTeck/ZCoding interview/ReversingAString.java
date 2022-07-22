c13e1e0291cbfdd96b4f8d8a85378aa5377ffb4b
FiodorG <gre_dorelus@yahoo.com>
2022-07-22 17:07:44 -0400

    Adding gh ecoments for readability

--- Java/NoorTeck/ZCoding interview/ReversingAString.java
+++ Java/NoorTeck/ZCoding interview/ReversingAString.java
@@ -6,15 +6,22 @@ public class ReversingAString {
     }
 
     public static String reverseString(String str) {
+
         String result = "";
 
         // Step 1. Convert string into charArray
         char[] strArr = str.toCharArray();
 
-        for (int i = strArr.length - 1; i >= 0; i--) {
-            System.out.println(strArr[i]);
-        }
+        // Step 2. Creating a temporary string
+        String tempString = "";
 
+        for (int i = strArr.length - 1; i >= 0; i--) {
+            // appending the elements to the temporary string
+            tempString = tempString + strArr[i];
+
+        }
+        // Printing the string backwards
+        System.out.println(tempString);
         return result;
     }
 }
