bc5180ba18247a43863e1c02b6e68dc62d881706
FiodorG <gre_dorelus@yahoo.com>
2022-08-02 12:29:22 -0400

    Playing arround with constructors

--- Java/CodingBat/Practice.java
+++ Java/CodingBat/Practice.java
@@ -1,20 +1,14 @@
 import java.util.Collections;
 
 public class Practice {
+
     public static void main(String[] args) {
 
         int[] myNumbers = { 2, 3, 5, 6, 7, 22, 88, 77 }; // declearing an array method 1
-        int[] myNumbers2 = new int[10]; // declearing an array method 2
 
-        System.out.println(myNumbers[4]);
-
-        Person obj = new Person();
-
-        Person obj1 = new Person("Michael", 10);
-
-        Person obj2 = new Person("Adam", 16, "DOAHDOUHDA34U");
-
-        // }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(myNumbers[i]);
+        }
 
     }
 
