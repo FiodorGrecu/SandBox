11502e58d561c687f604baa32e33167e3c4589f1
FiodorG <gre_dorelus@yahoo.com>
2022-08-02 15:13:44 -0400

    A problem that counts the nums of digits in an integer

--- Java/CodingBat/Practice.java
+++ Java/CodingBat/Practice.java
@@ -4,12 +4,27 @@ public class Practice {
 
     public static void main(String[] args) {
 
-        int[] myNumbers = { 2, 3, 5, 6, 7, 22, 88, 77 }; // declearing an array method 1
-
-        for (int i = 0; i < myNumbers.length; i++) {
-            System.out.println(myNumbers[i]);
-        }
+        int num = 1337;
+        System.out.println(getInt(num));
 
     }
 
+    public static int getInt(int num) {
+
+        String number = String.valueOf(num);
+
+        // int countDigit = 0;
+        // if (num < 2) {
+        // return num;
+        // }
+        // while (num != 0) {
+        // num = num / 10;
+        // countDigit += 1;
+
+        // }
+        // countDigit++;
+
+        // return countDigit;
+        return number.length();
+    }
 }
