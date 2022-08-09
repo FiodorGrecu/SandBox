13cec3c60e0525e40a93cb003b8f1fe5ab8251d9
FiodorG <gre_dorelus@yahoo.com>
2022-08-09 16:33:41 -0400

    Fernet key generator

--- Python_Encription/encription.py
+++ Python_Encription/encription.py
@@ -2,10 +2,11 @@ from cryptography.fernet import Fernet
 
 
 key = Fernet.generate_key()
+print(key)
 
-fernet = Fernet(key)
-message = "Super Cool"
-token = fernet.encrypt()
+# fernet = Fernet(key)
+# message = "Super"
+# token = fernet.encrypt()
 
-print("origional string:", message)
-print("encrypted string:", token)
+# print("origional string:", message)
+# print("encrypted string:", token)
