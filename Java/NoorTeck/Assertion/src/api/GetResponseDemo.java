7945961346eee4fef94fe84bbbbbb7ad9a431f07
FiodorG <gre_dorelus@yahoo.com>
2022-09-12 17:49:27 -0400

    Cleaning up everything

---
+++ Java/NoorTeck/Assertion/src/api/GetResponseDemo.java
@@ -0,0 +1,12 @@
+package asertion;
+
+public class GetResponseDemo {
+
+    public static void main(String[] args) {
+
+        String url = "https://us-zipcode.api.smartystreets.com/lookup";
+        System.out.println(url);
+
+    }
+
+}
