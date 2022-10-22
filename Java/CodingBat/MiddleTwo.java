84090f338337d4135febd10361ff96b52c5791a8
FiodorG <gre_dorelus@yahoo.com>
2022-10-21 23:20:57 -0400

    Coding bat Middle two problem

---
+++ Java/CodingBat/MiddleTwo.java
@@ -0,0 +1,48 @@
+public class MiddleTwo {
+    public static void main(String[] args) {
+        // test case 1
+        String str = "string";
+
+        String result = middleTwo(str);
+        System.out.println(result);
+
+        // test case 2
+        String str2 = "code";
+
+        String r2 = middleTwo(str2);
+        System.out.println(r2);
+
+        // test case 3
+        String str3 = "Practice";
+
+        String r3 = middleTwo(str3);
+        System.out.println(r3);
+
+    }
+
+    public static String middleTwo(String str) {
+        String result = "";
+        if (str.length() == 2) {
+            return str;
+        }
+        int len = str.length();
+        // keeping track of the half
+        int fHalf = len / 2;
+
+        // first half of the string
+        String firstHalfString = str.substring(0, fHalf);
+        // Last letter of the first half of the string
+        String lastLtrOfFirstHalf = firstHalfString.substring(firstHalfString.length() - 1);
+
+        // now we need to fing the second half of the
+        String secondHalfString = str.substring(fHalf);
+
+        // first letter of the second half of the string
+        String firstLtrOfSecondHalf = secondHalfString.substring(0, 1);
+
+        result = lastLtrOfFirstHalf + firstLtrOfSecondHalf;
+
+        return result;
+    }
+
+}
